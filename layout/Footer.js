6651f8dee3eaa9254235c57aa29f4067bba416b4
nickmac84 <nickmac84@gmail.com>
2021-08-17 13:49:15 +0100

    New Commit

---
+++ layout/Footer.js
@@ -0,0 +1,31 @@
+import { Link, withTranslation } from '../i18n'
+import styles from '../styles/layout.less'
+
+const Footer = ({ t }) => {
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.leftfont}>© 2021  Mojito All rights reserved.</div>
+            <ul>
+                <Link href="/">
+                    <li>
+                        <i className={styles.lemond}></i>
+                    </li>
+                </Link>
+                <li onClick={() => window.open("https://twitter.com/LemondFinance")}>
+                    <i className={styles.twitter}></i>
+                </li>
+                <li onClick={() => window.open("https://lemondfinance.medium.com")}>
+                    <i className={styles.medium}></i>
+                </li>
+                <li onClick={() => window.open("https://t.me/lemondok")}>
+                    <i className={styles.telegram}></i>
+                </li>
+                <li onClick={() => window.open("https://github.com/Lemond-finance/lemond")}>
+                    <i className={styles.github}></i>
+                </li>
+            </ul>
+        </footer>
+    )
+}
+
+export default withTranslation('header')(Footer)
